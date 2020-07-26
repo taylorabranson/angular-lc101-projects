18d3d9dc7d0452d1aeb04a7a289499fd08e74667
TaylorBranson <taylor.a.branson@gmail.com>
2020-07-26 16:39:34 -0500

    finished exercise 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -10,6 +10,7 @@ export class FavPhotosComponent implements OnInit {
   image1 = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstarwarsalways.files.wordpress.com%2F2014%2F05%2Fstwars2015002_cov.jpg&f=1&nofb=1';
   image2 = 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fvignette1.wikia.nocookie.net%2Fstarwars%2Fimages%2F6%2F67%2FSWKOTOR11-FC.jpg%2Frevision%2Flatest%3Fcb%3D20070116220336&f=1&nofb=1';
   image3 = 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Foyster.ignimgs.com%2Fwordpress%2Fstg.ign.com%2F2014%2F07%2FStar_Wars_1_Cassaday_cov-720x1067.jpg&f=1&nofb=1';
+  image4 = 'https://media.giphy.com/media/yYSSBtDgbbRzq/giphy.gif';
 
   constructor() { }
 

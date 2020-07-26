a85880fe9f660ec9568a8b3b070c559c1c2f5972
TaylorBranson <taylor.a.branson@gmail.com>
2020-07-26 16:04:55 -0500

    finished exercise part 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Favorite Photos';
+  image1 = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstarwarsalways.files.wordpress.com%2F2014%2F05%2Fstwars2015002_cov.jpg&f=1&nofb=1';
+  image2 = 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fvignette1.wikia.nocookie.net%2Fstarwars%2Fimages%2F6%2F67%2FSWKOTOR11-FC.jpg%2Frevision%2Flatest%3Fcb%3D20070116220336&f=1&nofb=1';
+  image3 = 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Foyster.ignimgs.com%2Fwordpress%2Fstg.ign.com%2F2014%2F07%2FStar_Wars_1_Cassaday_cov-720x1067.jpg&f=1&nofb=1';
 
   constructor() { }
 
